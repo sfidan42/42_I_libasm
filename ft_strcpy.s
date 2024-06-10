6931b1117257dde1319d88db81175614256b439b
sfidan42 <sadettinfidan00@gmail.com>
2024-06-10 17:24:58 +0300

    update

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -2,16 +2,16 @@ section .text
 global ft_strcpy
 
 ft_strcpy:
+	push rax			;	save rax
 loop:
 	cmp byte [rsi], 0	;   while (*src)
 	je end				;	{
-	push rax			;		save rax
 	mov al, byte [rsi]	;		al = *src
 	mov byte [rdi], al	;		*dst = al
-	pop rax				;		restore rax
 	inc rdi				;		dst++;
 	inc rsi				;		src++;
 	jmp loop			;	}
 end:
 	mov byte [rdi], 0	;	*dst = 0;
+	pop rax				;	restore rax
 	ret

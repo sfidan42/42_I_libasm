7eb0b1258b28356b6dbd8c7d804c34751e9d3fc3
sfidan42 <sadettinfidan00@gmail.com>
2024-06-10 18:30:31 +0300

    implement ft_list_size

--- code/main_bonus.c
+++ code/main_bonus.c
@@ -1,6 +1,7 @@
 #include <libasm.h>
 #include <libasm_bonus.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void	print(const char *s)
 {
@@ -19,20 +20,39 @@ t_list	*ft_list_new(void *data)
 	return (new);
 }
 
+void	print_list(t_list *list)
+{
+	print("List content:\n");
+	while (list)
+	{
+		print(" -- ");
+		print(list->data);
+		print("\n");
+		list = list->next;
+	}
+}
+
+void	free_list(t_list *list)
+{
+	t_list	*aux;
+
+	while (list)
+	{
+		aux = list->next;
+		free(list);
+		list = aux;
+	}
+}
+
 int	main(void)
 {
 	t_list	*head;
-	t_list	*new;
 
 	head = NULL;
-	new = ft_list_new("Hello\n");
-	ft_list_push_front(&head, new);
-	print(head->data);
-	new = ft_list_new("World\n");
-	ft_list_push_front(&head, new);
-	print(head->data);
-	print(head->next->data);
-	free(head->next);
-	free(head);
+	ft_list_push_front(&head, ft_list_new("Hello"));
+	ft_list_push_front(&head, ft_list_new("World"));
+	print_list(head);
+	printf("Size of the list: %d\n", ft_list_size(head));
+	free_list(head);
 	return (0);
 }
---
+++ code/src/ft_list_size_bonus.s
@@ -0,0 +1,17 @@
+section .text
+global ft_list_size
+
+ft_list_size:
+	; Input:
+	;	rdi = t_list* lst
+	; Output:
+	;	eax = size of the list
+	mov rax, 0			;	rax = 0;
+loop:					;	while (true) {
+	cmp rdi, 0			;		if (rdi == NULL)
+	je end				;			break ;
+	inc rax				;		rax++ ;
+	mov rdi, [rdi + 8]	;		rdi = rdi->next
+	jmp loop			;	}
+end:					;	return (rax) ;
+	ret

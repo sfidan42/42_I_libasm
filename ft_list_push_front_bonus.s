1432b06a37d4eb095a62c6ff2944af47c68cb7e3
sfidan42 <sadettinfidan00@gmail.com>
2024-06-10 12:09:10 +0300

    implement push_front

--- ft_list_push_front_bonus.s
+++ ft_list_push_front_bonus.s
@@ -2,11 +2,16 @@ section .text
 global ft_list_push_front
 
 ft_list_push_front:
-    cmp rdi, 0
-    je  end
-    push qword [ rdi ]
-    mov [ rdi ], rsi
-    pop qword rsi
-    mov [ rdi + 8 ], rsi
+	; Input:
+	; rdi - address of pointer to head of list (t_list **alst)
+	; rsi - pointer to new node (t_list *new)
+
+	cmp rsi, 0			;	if (!new)
+	je end				;		return
+	cmp rdi, 0			;	if (!alst)
+	je end				;		return
+	mov rax, [rdi]		;	rax = *alst
+	mov [rsi + 8], rax	;	new->next = rax
+	mov [rdi], rsi		;	*alst = new
 end:
-    ret
+	ret

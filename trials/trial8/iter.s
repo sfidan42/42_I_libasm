eb082570a94d4cb4a2a69cff9b870848c8aa3e82
sfidan42 <sadettinfidan00@gmail.com>
2024-07-05 16:35:06 +0300

    implement list iter

--- trials/trial8/iter.s
+++ trials/trial8/iter.s
@@ -7,21 +7,31 @@ ft_list_sort:
 	;	rsi: int (*cmp)()
 	push rbp
 	mov rbp, rsp
-	sub rsp, 0x8
+	sub rsp, 0x10
 	
-	mov rax, [rdi]			;	rax = *begin_list
-	mov [rbp - 0x8], rax	;	ptr = rax
-
-l1:							;	while (1) {
-	cmp qword [rbp - 0x8], 0			;	if (ptr == 0)
-	je e1						;		break ;
-
-
-
-	mov rax, [rbp - 0x8]		;	rax = ptr
-	mov rax, [rax + 0x8]		;	rax = rax->next
+	mov rax, [rdi]				;	rax = *begin_list
 	mov [rbp - 0x8], rax		;	ptr = rax
-	jmp l1					;	}
+	mov [rbp - 0x10], rsi		;	backup cmp
+
+l1:
+	mov rax, [rbp - 0x8]		;	rax = ptr
+	mov rax, [rax + 0x8]		;	rax = ptr->next
+	cmp rax, 0x0				;	while (ptr->next)
+	je e1						;	{
+
+	mov rsi, [rbp - 0x8]		;		rdi = ptr
+	mov rsi, [rsi + 0x8]		;		rdi = ptr->next
+	mov rsi, [rsi]				;		rdi = ptr->next->data
+	mov rdi, [rbp - 0x8]		;		rsi = ptr
+	mov rdi, [rdi]				;		rsi = ptr->data
+
+	mov eax, 0x0				;		eax = 0
+	call [rbp - 0x10]			;		cmp(ptr->data, ptr->next->data)
+
+	mov rax, [rbp - 0x8]		;		rax = ptr
+	mov rax, [rax + 0x8]		;		rax = rax->next
+	mov [rbp - 0x8], rax		;		ptr = rax
+	jmp l1						;	}
 e1:
 
 	leave
